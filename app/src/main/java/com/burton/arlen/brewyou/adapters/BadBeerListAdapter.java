a2ad0f01b86d1017cef09a58320e5530d2e5ef13
arlen <burton.arlen.1990@gmail.com>
2017-06-02 11:39:40 -0700

    refactor code to implement viewonclicklistener, as well as take and validate data from the EditText in the mainactivity

---
+++ app/src/main/java/com/burton/arlen/brewyou/adapters/BadBeerListAdapter.java
@@ -0,0 +1,8 @@
+package com.burton.arlen.brewyou.adapters;
+
+/**
+ * Created by arlen on 6/2/17.
+ */
+
+public class BadBeerListAdapter {
+}
--- app/src/main/java/com/burton/arlen/brewyou/ui/MainActivity.java
+++ app/src/main/java/com/burton/arlen/brewyou/ui/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.burton.arlen.brewyou.R;
@@ -13,13 +14,14 @@ import com.burton.arlen.brewyou.R;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.aboutPageButton) Button mAboutPageButton;
     @Bind(R.id.youBrewPageButton) Button mYouBrewPageButton;
     @Bind(R.id.notYouBrewPageButton) Button mNotYouBrewPageButton;
     @Bind(R.id.searchBeerButton) Button mSearchBeerButton;
     @Bind(R.id.welcomeText) TextView mWelcomeText;
     @Bind(R.id.taglineText) TextView mTaglineText;
+    @Bind(R.id.beerSearchEnter) EditText mBeerSearchEnter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,34 +33,37 @@ public class MainActivity extends AppCompatActivity {
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/font.ttf");
         mWelcomeText.setTypeface(font);
         mTaglineText.setTypeface(font);
+        mAboutPageButton.setOnClickListener(this);
+        mNotYouBrewPageButton.setOnClickListener(this);
+        mYouBrewPageButton.setOnClickListener(this);
+        mSearchBeerButton.setOnClickListener(this);
+    }
 
-        mAboutPageButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AboutApp.class);
-                startActivity(intent);
-            }
-        });
-        mYouBrewPageButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, YouBrews.class);
-                startActivity(intent);
-            }
-        });
-        mNotYouBrewPageButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, NotYouBrews.class);
-                startActivity(intent);
-            }
-        });
-        mSearchBeerButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
+    @Override
+    public void onClick(View v) {
+        if (v == mSearchBeerButton){
+            String beerSearchTerm = mBeerSearchEnter.getText().toString();
+            if (beerSearchTerm.trim().length() != 0){
                 Intent intent = new Intent(MainActivity.this, BrewSearch.class);
+                intent.putExtra("beerSearchTerm", beerSearchTerm);
                 startActivity(intent);
+            } else {
+                mBeerSearchEnter.setError("You gotta look for something before you can find it");
+                mBeerSearchEnter.setText("");
             }
-        });
+        }
+        if (v == mAboutPageButton){
+            Intent intent = new Intent(MainActivity.this, AboutApp.class);
+            startActivity(intent);
+        }
+        if(v == mYouBrewPageButton){
+            Intent intent = new Intent(MainActivity.this, YouBrews.class);
+            startActivity(intent);
+        }
+        if(v == mNotYouBrewPageButton){
+            Intent intent = new Intent(MainActivity.this, NotYouBrews.class);
+            startActivity(intent);
+        }
+
     }
 }
