1ffddcfebdf334355fbe7c360dda2dbaed917dd1
arlen <burton.arlen.1990@gmail.com>
2017-06-02 11:03:35 -0700

    update gradle and fiddle with sdk build tools compatability with api 25

---
+++ app/src/main/java/com/burton/arlen/brewyou/models/Beer.java
@@ -0,0 +1,8 @@
+package com.burton.arlen.brewyou.models;
+
+/**
+ * Created by arlen on 6/2/17.
+ */
+
+public class Beer {
+}
---
+++ app/src/main/java/com/burton/arlen/brewyou/services/BottleService.java
@@ -0,0 +1,8 @@
+package com.burton.arlen.brewyou.services;
+
+/**
+ * Created by arlen on 6/2/17.
+ */
+
+public class BottleService {
+}
