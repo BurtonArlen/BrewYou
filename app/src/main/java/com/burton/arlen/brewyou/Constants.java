b9c4550b7234f6a79feef68fcb3e0699f7619d6b
arlen <burton.arlen.1990@gmail.com>
2017-06-02 14:32:27 -0700

    write request for breweryDB

---
+++ app/src/main/java/com/burton/arlen/brewyou/Constants.java
@@ -0,0 +1,18 @@
+package com.burton.arlen.brewyou;
+
+/**
+ * Created by arlen on 6/2/17.
+ */
+
+public class Constants {
+    public  static final String BEER_KEY = BuildConfig.BEERKEY;
+    public  static  final String BASE_URL= "http://api.brewerydb.com/v2/search/";
+    public static final String KEY_PARAM = "key";
+    public static final String ENDPOINT_QUERY = "q";
+    public static final String ENDPOINT_KEYWORD = "type";
+    public static final String ENDPOINT_TYPE = "beer";
+
+
+
+}
+http://api.brewerydb.com/v2/search/?q=Rainer&type=beer&key=57dcc32caa34e7ea24a812532cb63716
--- app/src/main/java/com/burton/arlen/brewyou/services/BottleService.java
+++ app/src/main/java/com/burton/arlen/brewyou/services/BottleService.java
@@ -1,8 +1,29 @@
 package com.burton.arlen.brewyou.services;
 
+import com.burton.arlen.brewyou.Constants;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 /**
  * Created by arlen on 6/2/17.
  */
 
 public class BottleService {
+    public static void findBeer(String beerName, Callback callback){
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.ENDPOINT_QUERY, beerName)
+                .addQueryParameter(Constants.ENDPOINT_KEYWORD, Constants.ENDPOINT_TYPE)
+                .addQueryParameter(Constants.KEY_PARAM, Constants.BEER_KEY);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder().url(url).build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
 }
