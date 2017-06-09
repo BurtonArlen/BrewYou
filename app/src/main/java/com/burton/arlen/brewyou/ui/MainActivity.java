5b4ff6c5c9b538269a6fba069ca02a1d67b7a0ef
arlen <burton.arlen.1990@gmail.com>
2017-06-08 20:22:11 -0700

    get google user profile picture

--- app/src/main/java/com/burton/arlen/brewyou/ui/MainActivity.java
+++ app/src/main/java/com/burton/arlen/brewyou/ui/MainActivity.java
@@ -1,7 +1,9 @@
 package com.burton.arlen.brewyou.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +27,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -31,14 +35,22 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private FirebaseAuth mAuth;
     private GoogleApiClient mGoogleApiClient;
+    private Context mContext;
+    private static final int MAX_WIDTH = 300;
+    private static final int MAX_HEIGHT = 300;
+
+
 
     @Bind(R.id.youBrewPageButton) Button mYouBrewPageButton;
     @Bind(R.id.notYouBrewPageButton) Button mNotYouBrewPageButton;
+    @Bind(R.id.imageLogin) ImageView mProfilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context mContext = getApplicationContext();
 
         ButterKnife.bind(this);
 
@@ -47,8 +59,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mAuth = FirebaseAuth.getInstance();
         String userName = mAuth.getCurrentUser().getDisplayName();
+        Uri profilePic = mAuth.getCurrentUser().getPhotoUrl();
         setTitle("BrewYou: " + userName + "'s Profile");
 
+        Picasso.with(mContext).load(profilePic)
+                .resize(MAX_WIDTH, MAX_HEIGHT)
+                .centerCrop()
+                .into(mProfilePic);
     }
 
     private void revokeAccess() {
