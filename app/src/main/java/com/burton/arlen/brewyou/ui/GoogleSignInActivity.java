be4709fd97e6db1acf8ebcf8aa46dc915722f9e5
Guest User <Guest@Epicodus-5D.local>
2017-06-15 08:37:09 -0700

    add landscape orientation for sign in activity

--- app/src/main/java/com/burton/arlen/brewyou/ui/GoogleSignInActivity.java
+++ app/src/main/java/com/burton/arlen/brewyou/ui/GoogleSignInActivity.java
@@ -4,24 +4,18 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Typeface;
-import android.os.CountDownTimer;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.burton.arlen.brewyou.Constants;
 import com.burton.arlen.brewyou.R;
-import com.burton.arlen.brewyou.util.ItemTouchHelperViewHolder;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -37,10 +31,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.Transaction;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
 public class GoogleSignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
 
@@ -59,18 +49,19 @@ public class GoogleSignInActivity extends AppCompatActivity implements GoogleApi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_google_sign_in);
+        mOrientation = getResources().getConfiguration().orientation;
+        if (mOrientation == Configuration.ORIENTATION_LANDSCAPE){
+            setContentView(R.layout.activity_google_sign_in_land);
+        } else {
+            setContentView(R.layout.activity_google_sign_in);
+        }
         createProgressDialog();
-
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/font.ttf");
-
         mImageLogin = (ImageView) findViewById(R.id.imageLogin);
         mTitle = (TextView) findViewById(R.id.welcomeText);
         mTag = (TextView) findViewById(R.id.taglineText);
-
         mTag.setTypeface(font);
         mTitle.setTypeface(font);
-
         findViewById(R.id.sign_in_button).setOnClickListener(this);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
